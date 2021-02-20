6a98c2637be41422df9c227cda7d4232433141c2
lhr <jiro110516@gmail.com>
2021-02-20 17:43:59 +0900

    OrderDao 주문sql

--- SUBWAYProject/src/dao/OrderDao.java
+++ SUBWAYProject/src/dao/OrderDao.java
@@ -1,5 +1,33 @@
 package dao;
 
+import java.util.List;
+import java.util.Map;
+
+import util.JDBCUtil;
+
 public class OrderDao {
+	
+//	싱글톤 패턴 으로 제작
+	
+//	생성자 만듬(private)
+	private OrderDao(){}
+	
+//	객체를 보관할 변수 생성
+	private static OrderDao instance;
+	
+//	메서드 호출 시 객체 주소 부여 
+	public static OrderDao getInstance(){
+		if(instance == null){
+			instance = new OrderDao();
+		}
+		return instance;
+	}
+	
+	private JDBCUtil jdbc = JDBCUtil.getInstance();
+
+	public List<Map<String, Object>> selectorderList() {
+		String sql = null;
+		return jdbc.selectList(sql);
+	}
 
 }
