caf0939a29db450d5151aced4fbaaf532d6c083b
lhr <jiro110516@gmail.com>
2021-02-20 17:44:14 +0900

    OrderService 주문서비스

--- SUBWAYProject/src/service/OrderService.java
+++ SUBWAYProject/src/service/OrderService.java
@@ -1,7 +1,55 @@
 package service;
 
+import dao.OrderDao;
+import util.ScanUtil;
+import util.View;
+
 public class OrderService {
-	public static void main(String[] args) {
-		System.out.println("OrderService");
+//	생성자 만듬(private)
+	private OrderService(){}
+	
+//	객체를 보관할 변수 생성
+	private static OrderService instance;
+	
+//	메서드 호출 시 객체 주소 부여 
+	public static OrderService getInstance(){
+		if(instance == null){
+			instance = new OrderService();
+		}
+		return instance;
+	}
+	
+	private OrderDao orderDao = OrderDao.getInstance();
+	
+	public void orderhome(){
+		
+		System.out.println("--------------------------------------");
+		System.out.println("1.주문목록 조회\t2.주문등록\t3.이전으로");
+		System.out.println("--------------------------------------");
+		System.out.print("번호 입력>");
+		int input = ScanUtil.nextInt();
+		switch (input) {
+		case 1:
+			orderList();
+			break;
+		case 2:
+			orderReg();
+			break;
+		case 3:
+//			return View.ORDER; 
+
+		
+		}
+		
+}
+
+	private void orderReg() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void orderList() {
+		// TODO Auto-generated method stub
+		
 	}
 }
