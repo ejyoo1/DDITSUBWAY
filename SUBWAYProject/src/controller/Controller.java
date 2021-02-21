bb43c109cd605b3844917ed4deb4abac41ef5cd7
ejyoo1 <o3k3585@gmail.com>
2021-02-21 09:40:08 +0900

    Revert "메뉴 서비스 세팅"
    This reverts commit 93445df007b7229a478d6b149d9163c3d7eac4b6.

--- SUBWAYProject/src/controller/Controller.java
+++ SUBWAYProject/src/controller/Controller.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import service.BoardService;
 import service.LoginMainMenuService;
-import service.MenuService;
 import service.MyService;
 import service.UserService;
 import util.ScanUtil;
@@ -31,7 +30,6 @@ public class Controller {
 	private BoardService boardService = BoardService.getInstance();
 	private MyService myService = MyService.getInstance();
 	private LoginMainMenuService loginMainMenuService = LoginMainMenuService.getInstance();
-	private MenuService menuService = MenuService.getInstance();
 
 	private void start() {
 		int view = View.HOME;
@@ -42,7 +40,6 @@ public class Controller {
 				case View.LOGIN: view = userService.login(); break;
 				case View.JOIN: view = userService.join(); break;
 				case View.LOGIN_MAIN_MENU: view = loginMainMenuService.loginMainHome(); break;
-				case View.MENU: view = menuService.menuHome();break;
 				case View.MYPAGE_MENU: view = myService.start(); break;
 				case View.BOARD_LIST: view = boardService.boardList(); break;
 			}
--- SUBWAYProject/src/service/MenuService.java
+++ SUBWAYProject/src/service/MenuService.java
@@ -1,19 +1,8 @@
 package service;
 
-import util.View;
-
 public class MenuService {
 	//메뉴서비스
-	private MenuService(){}
-	private static MenuService instance;
-	public static MenuService getInstance(){
-		if(instance == null){
-			instance = new MenuService();
-		}
-		return instance;
-	}
-	public int menuHome() {
-		
-		return View.MENU;
+	public static void main(String[] args) {
+		System.out.println("Menuservice");
 	}
 }
