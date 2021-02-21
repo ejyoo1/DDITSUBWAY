73c55194061c03367ec7f5092ae1f7fde5a737b6
ejyoo1 <o3k3585@gmail.com>
2021-02-21 13:58:52 +0900

    페이지 상수 주석 추가 및 고객센터 상수 생성

--- SUBWAYProject/src/util/View.java
+++ SUBWAYProject/src/util/View.java
@@ -2,16 +2,17 @@ package util;
 
 public class View {
 
-	public static final int HOME = 0;
-	public static final int LOGIN = 1;
-	public static final int JOIN = 2;
-	public static final int LOGIN_MAIN_MENU = 3;
-	public static final int MENU= 4;//관리자가 아닌 경우 메뉴만출력,관리자인 경우 전체조회와 메뉴출력 
-	public static final int NOTICE_LIST = 5;
-	public static final int EVENT_LIST = 6;
-	public static final int ORDER_LIST = 7;
-	public static final int MYPAGE_MENU = 8;//로그인 정보 판별 후 메뉴 다르게 주어야 함.
-	public static final int FRANC_LIST = 9;
+	public static final int HOME = 0;//프로그램 실행 시 메인 메뉴 출력 페이지 
+	public static final int LOGIN = 1;//로그인 페이지 
+	public static final int JOIN = 2;//회원가입 페이지 
+	public static final int LOGIN_MAIN_MENU = 3;//로그인 시 메인 메뉴 페이지(로그인 상태별로 표시) 
+	public static final int MENU= 4;//메뉴 페이지 관리자가 아닌 경우 메뉴만출력,관리자인 경우 전체조회와 메뉴출력 
+	public static final int NOTICE_LIST = 5;//공지사항 페이지
+	public static final int EVENT_LIST = 6;//이벤트 페이지 
+	public static final int ORDER_LIST = 7;//주문 페이지 
+	public static final int MYPAGE_MENU = 8;//마이페이지 페이지 로그인 정보 판별 후 메뉴 다르게 주어야 함.
+	public static final int FRANC_LIST = 9;//가맹점 관리 페이지 
+	public static final int INQU_LIST = 10;//고객센터 관리 페이지 
 	public static final int BOARD_LIST = 3333;
 	public static final int BOARD_UPDATE = 4444;
 	
