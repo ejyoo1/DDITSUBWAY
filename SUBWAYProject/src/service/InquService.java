97b9155787599975fb17c133f38959468131f7fa
ejyoo1 <o3k3585@gmail.com>
2021-02-21 14:24:46 +0900

    고객 센터 관리 서비스 코드 생성 InquService
    1) 해커톤 적용
    2) 빈 메서드 생성

--- SUBWAYProject/src/service/InquService.java
+++ SUBWAYProject/src/service/InquService.java
@@ -1,5 +1,22 @@
 package service;
 
-public class InquService {
+import util.View;
 
+public class InquService {
+	//1:1 문의 관리 서비스 
+	private InquService(){}
+	private static InquService instance;
+	public static InquService getInstance(){
+		if(instance == null){
+			instance = new InquService();
+		}
+		return instance;
+	}
+	
+	public int inquList() {
+		
+		return View.INQU_LIST;
+	}
+	//고객센터 관리 서비스 
+	
 }
