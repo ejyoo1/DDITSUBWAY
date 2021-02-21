d2ae59771f86d912a6e1b2b1f9466372282766d6
ejyoo1 <o3k3585@gmail.com>
2021-02-21 14:21:05 +0900

    FranService 세팅
    1) 해커톤 적용
    2) 메서드 생성

--- SUBWAYProject/src/service/FrancService.java
+++ SUBWAYProject/src/service/FrancService.java
@@ -1,5 +1,19 @@
 package service;
 
+import util.View;
+
 public class FrancService {
 // 가맹점 관리 서비스
+	private FrancService(){}
+	private static FrancService instance;
+	public static FrancService getInstance(){
+		if(instance == null){
+			instance = new FrancService();
+		}
+		return instance;
+	}
+	public int francList() {
+		
+		return View.EVENT_LIST;
+	}
 }
