0f4c4f8e569e0d049729dedf0f85a667ffbab39e
ejyoo1 <o3k3585@gmail.com>
2021-02-21 14:03:06 +0900

    NoticeService 생성
    1) 해커톤 패턴 적용
    2) 빈 메서드 생성

--- SUBWAYProject/src/service/NotiService.java
+++ SUBWAYProject/src/service/NotiService.java
@@ -1,8 +1,21 @@
 package service;
 
+import util.View;
+
 public class NotiService {
 	//공지 서비스 
-	public static void main(String[] args) {
-		System.out.println("NotiService");
+	private NotiService(){}
+	private static NotiService instance;
+	public static NotiService getInstance(){
+		if(instance == null){
+			instance = new NotiService();
+		}
+		return instance;
 	}
+	public int noticeHome() {
+		
+		return View.NOTICE_LIST;
+	}
+	
+	
 }
