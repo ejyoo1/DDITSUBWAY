c3dcfb6e593e06413737ce06d5dde1024441a249
lhr <jiro110516@gmail.com>
2021-02-22 21:43:38 +0900

    OrederDao select orderList의 주문목록 조회 주문목록조회 완료

--- SUBWAYProject/src/dao/OrderDao.java
+++ SUBWAYProject/src/dao/OrderDao.java
@@ -27,21 +27,38 @@ public class OrderDao {
 
 	public List<Map<String, Object>> selectOrderList() {
 		String sql = "SELECT A.ORDER_NO"
-				+ "        , (SELECT B.BUYER_NAME FROM WHERE BUYER B B.BUYER_ID = A.BUYER_ID)"
+				+ "        , B.BUYER_NAME"
 				+ "        , D.MENU_NM"
-				+ "        , A.ORDER,MEMBER_DATE "
-				+ " FROM ORDER A INNER JOIN INFO_ORDER C ON (C.ORDER_NO = A.ORDER_NO)"
-				+ "              INNER JOIN MENU D ON (D.MENU_NO = C.MENU_NO)";
+				+ "        , TO_CHAR(A.ORDER_MEMBER_DATE,'YY-MM-DD')"
+				+ "   FROM ORDERS A INNER JOIN BUYER B      ON(A.BUYER_ID = B.BUYER_ID)"
+				+ "                 INNER JOIN INFO_ORDER C ON(A.ORDER_NO = C.ORDER_NO)"
+				+ "                 INNER JOIN MENU D       ON(C.MENU_NO_SEQ = D.MENU_NO_SEQ)";
 		return jdbc.selectList(sql);
 	}
 	
-	public Map<String, Object> selectOrderOne(){//[주문번호, 가맹점명, 메뉴이름, 재료(선택), 수량, 주문일(회원), 주문일(가맹점 확인)]
-		String sql = "SELECT ORDER_NO"
-				+ "        , (SELECT B.BUYER_NAME FORM BUYER B B.BUYER_ID = A.BUYER_ID)"
-				+ "        , (SELECT MENU_NM FROM ORD,";
+	public List<Map<String, Object>> selectOrderList2(String orderNo){//[주문번호, 가맹점명, 메뉴이름, 재료(선택), 수량, 주문일(회원), 주문일(가맹점 확인)]
+		String sql = "SELECT  A.ORDER_NO "
+				+ "        ,  B.BUYER_NAME "
+				+ "        ,  F.MENU_NM "
+				+ "        ,  E.INGR_NAME "
+				+ "        ,  C.INFO_CART_QTY "
+				+ "        ,  TO_CHAR(A.ORDER_MEMBER_DATE,'YY-MM-DD') "
+				+ "        ,  TO_CHAR(A.ORDER_BUYER_CHOICE,'YY-MM-DD') "
+				+ "   FROM ORDERS A   INNER JOIN BUYER B      ON(A.BUYER_ID = B.BUYER_ID) "
+				+ "                   INNER JOIN INFO_ORDER C ON(A.ORDER_NO = C.ORDER_NO) "
+				+ "                   INNER JOIN ADD_INGR D   ON(C.INFO_ORDER_NO = D.INFO_ORDER_NO) "
+				+ "                   INNER JOIN INGR E       ON(D.INGR_NO = E.INGR_NO) "
+				+ "                   INNER JOIN MENU F       ON(C.MENU_NO_SEQ = F.MENU_NO_SEQ)"
+				+ "   WHERE A.ORDER_NO = ?";
 		List<Object> param = new ArrayList<>();
-		param.add("ORDER_NO");
-		return jdbc.selectOne(sql, param);
+		param.add(orderNo);
+		return jdbc.selectList(sql, param);
+	}
+
+	public List<Map<String, Object>> slectBuyerList() {
+		String sql = "SELECT BUYER_NAME"
+				+ "   FROM   BUYER";
+		return jdbc.selectList(sql);
 	}
 
 }
