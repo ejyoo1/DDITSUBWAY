d8a512b0dbf8359708ad0078aaad004a03660175
ejyoo1 <o3k3585@gmail.com>
2021-02-22 12:53:15 +0900

    유저 로그인 구현 완료
    1) UserService.java 구현완료
    2) LoginMainMenuService.java 구현완료
    3) 참고 사항 : 유저 타입 별 유저id, 유저pw, 유저 테이블 정보 Controller 에 저장

--- SUBWAYProject/src/service/LoginMainMenuService.java
+++ SUBWAYProject/src/service/LoginMainMenuService.java
@@ -1,5 +1,6 @@
 package service;
 
+import controller.Controller;
 import util.ScanUtil;
 import util.View;
 
@@ -12,7 +13,36 @@ public class LoginMainMenuService {
 		}
 		return instance;
 	}
+//	로그인 성공 시 메뉴 출력되는 메서드
 	public int loginMainHome() {
+		
+		System.out.println("입력>");
+		System.out.println(Controller.loginUser);
+		System.out.println("==MAIN MENU==");
+//		로그인 코드 별로 메뉴 출력
+		if(Controller.loginUser.get("LOGIN_CODE").equals(1)) {
+			System.out.println("고객 메뉴 출력");/////////////////////////////////////////////////////////////////////////나중에 제거할 라인
+			System.out.println("[1. 메뉴소개   2. 공지사항   3. 이벤트   4. 주문   5. 마이페이지   6. 로그아웃]");
+			int userInput = ScanUtil.nextInt();
+			switch(userInput) {
+				case 1: return View.MENU;
+				case 2: return View.NOTICE_LIST;
+				case 3: return View.EVENT_LIST;
+				case 4: return View.ORDER_MENU;
+				case 5: return View.MYPAGE_MENU;
+				case 6: Controller.loginUser = null; return View.HOME;
+			}
+		}else if(Controller.loginUser.get("LOGIN_CODE").equals(2)) {
+			System.out.println("가맹점 메뉴 출력");///////////////////////////////////////////////////////////////////////나중에 제거할 라인
+			System.out.println("[1. 메뉴소개   2. 공지사항   3. 이벤트   4. 주문   5. 마이페이지   6. 로그아웃]");
+		}else if(Controller.loginUser.get("LOGIN_CODE").equals(3)) {
+			System.out.println("관리자 메뉴 출력");///////////////////////////////////////////////////////////////////////나중에 제거할 라인
+			System.out.println("[1. 메뉴관리   2. 공지관리   3. 이벤트 관리   4. 가맹점 관리   5. 고객 센터 관리   6. 로그아웃]");
+		}else {
+			System.out.println("로그인 코드 정보없음");
+		}
+		
+		
 		return View.LOGIN_MAIN_MENU;
 	}
 }
--- SUBWAYProject/src/service/UserService.java
+++ SUBWAYProject/src/service/UserService.java
@@ -58,26 +58,42 @@ public class UserService {
 		String table = "오류";
 		String idColumn = "MEM_ID";
 		String pwColumn = "MEM_PW";
+		int loginCode = 1;
+//		로그인 타입 별 작업
 		while(table.equals("오류")) {
 			System.out.println("로그인 타입 입력 > 1. 고객\t2. 가맹점\t3. 관리자");
 			int loginType = ScanUtil.nextInt();
 			switch(loginType) {
-				case 1: table = "MEMBER"; idColumn = "MEM_ID"; pwColumn="MEM_PW"; break;
-				case 2: table = "BUYER"; idColumn = "BUYER_ID"; pwColumn="BUYER_PW"; break;
-				case 3: table = "MANAGER"; idColumn = "MANAGER_ID"; pwColumn="MANAGER_PW"; break;
+//			로그인 타입이 고객일 때, 로그인 코드, 유저 테이블명, 유저 아이디, 유저 패스워드 저장
+				case 1: table = "MEMBER"; idColumn = "MEM_ID"; pwColumn="MEM_PW"; loginCode=1; break;
+				case 2: table = "BUYER"; idColumn = "BUYER_ID"; pwColumn="BUYER_PW"; loginCode=2; break;
+				case 3: table = "MANAGER"; idColumn = "MANAGER_ID"; pwColumn="MANAGER_PW"; loginCode=3; break;
 				default:
 					System.out.println("다시입력"); table = "오류"; break;
 			}
 		}
 		
 		Map<String, Object> user = userDao.selectUser(userId, password, table, idColumn, pwColumn);
-		
+//		유저 로그인 타입에 따라 로그인 정보에 유저 로그인 코드와 해당하는 유저 테이블 명을 저장
+//		로그인 시 로그인 된 유저가 존재하는 경우에만 로그인 테이블과 로그인 코드를 저장
 		if(user == null){
 			System.out.println("아이디 혹은 비밀번호를 잘못 입력하셨습니다.");
 		}else{
 			System.out.println("로그인 성공");
+			if(table.equals("MEMBER")) {
+				user.put("LOGIN_TABLE", table);
+				user.put("LOGIN_CODE", loginCode);
+			}else if(table.equals("BUYER")) {
+				user.put("LOGIN_TABLE", table);
+				user.put("LOGIN_CODE", loginCode);
+			}else if(table.equals("MANAGER")) {
+				user.put("LOGIN_TABLE", table);
+				user.put("LOGIN_CODE", loginCode);
+			}else {
+				System.out.println("정보없음");
+			}
 			Controller.loginUser = user;
-			return View.BOARD_LIST;
+			return View.LOGIN_MAIN_MENU;
 		}
 		
 		return View.HOME;
