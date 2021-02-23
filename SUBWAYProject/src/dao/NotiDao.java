c4b4f6358326ff9ccc99846fcd37c2858de80903
ejyoo1 <o3k3585@gmail.com>
2021-02-23 10:55:11 +0900

    notiDao sql 정리

--- SUBWAYProject/src/dao/NotiDao.java
+++ SUBWAYProject/src/dao/NotiDao.java
@@ -19,19 +19,27 @@ public class NotiDao {
 //	중요 공지사항 목록 조회
 	private JDBCUtil jdbc = JDBCUtil.getInstance();
 	public List<Map<String, Object>> selectImportantNotiList() {
-		String sql = " SELECT * FROM NOTICE WHERE NOTICE_IMPOR_CODE = 0 ORDER BY NOTICE_REG_DATE";
+		String sql = " SELECT * "
+				+ "FROM NOTICE "
+				+ "WHERE NOTICE_IMPOR_CODE = 0 "
+				+ "ORDER BY NOTICE_REG_DATE";
 		return jdbc.selectList(sql);
 	}
 	
 //	일반 공지사항 목록 조회
 	public List<Map<String, Object>> selectNotiAllList() {
-		String sql = "SELECT * FROM NOTICE WHERE NOTICE_IMPOR_CODE = 1 ORDER BY NOTICE_REG_DATE";
+		String sql = "SELECT * "
+				+ "FROM NOTICE "
+				+ "WHERE NOTICE_IMPOR_CODE = 1 "
+				+ "ORDER BY NOTICE_REG_DATE";
 		return jdbc.selectList(sql);
 	}
 	
 //	상세 공지사항 조회
 	public Map<String, Object> selectNotiList(Map<String, Object> param) {
-		String sql = "SELECT * FROM NOTICE WHERE NOTICE_NO = ?";
+		String sql = "SELECT * "
+				+ "FROM NOTICE "
+				+ "WHERE NOTICE_NO = ?";
 		List<Object> p = new ArrayList<>();
 		p.add (param.get ("NOTICE_NO"));
 		return jdbc.selectOne(sql, p);
