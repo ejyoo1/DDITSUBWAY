9951f2b7014b51a255d2e0ae66a371349e760fbd
ejyoo1 <o3k3585@gmail.com>
2021-02-24 15:35:14 +0900

    [Improvement] 아이디, 전화번호 유효성 검사 추가

--- SUBWAYProject/src/util/VerifiedUtil.java
+++ SUBWAYProject/src/util/VerifiedUtil.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class VerifiedUtil {
 	private VerifiedUtil () {
 		
@@ -23,8 +26,33 @@ public class VerifiedUtil {
 		return flag;
 	}
 	
-	//아이디 정규화
-	//이메일 정규화
-	//비밀번호 정규화
+
+
+
+//	아이디 정규식	
+	public boolean verifiedId(String userId) {
+		String regex1 = "[a-zA-Z0-9-_]{5,20}+";//아이디 정규식 : 특수문자 -_만
+		Pattern p = Pattern.compile (regex1);
+		Matcher m = p.matcher (userId);
+		System.out.println ("아이디 정규식 결과 : " + m.matches());
+		return m.matches();		
+	}
 	
+//	비밀번호 정규식
+	public boolean verifiedNumber(String userNumber) {
+		String regex1 = "^\\d{3}-\\d{3,4}-\\d{4}$";//전화번호 정규식
+		Pattern p = Pattern.compile (regex1);
+		Matcher m = p.matcher (userNumber);
+		System.out.println ("전화번호 정규식 결과 : " + m.matches());
+		return m.matches();
+	}
+	
+//	이메일 정규식
+	public boolean verifiedEmail(String userEmail) {
+		String regex1 = "^[0-9a-zA-Z]([-_]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";//이메일 정규식
+		Pattern p = Pattern.compile (regex1);
+		Matcher m = p.matcher (userEmail);
+		System.out.println ("이메일 정규식 결과 : " + m.matches());
+		return m.matches();
+	}
 }
