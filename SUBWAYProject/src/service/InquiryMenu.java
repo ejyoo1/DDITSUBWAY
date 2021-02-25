62e5e04ca22f3d8eb507d705fa0d2184f4bb48cd
Mulltung <79069668+Mulltung@users.noreply.github.com>
2021-02-25 13:57:13 +0900

    MyService, InquiryMenu 수정
    1) 출력코드 수정

--- SUBWAYProject/src/service/InquiryMenu.java
+++ SUBWAYProject/src/service/InquiryMenu.java
@@ -26,14 +26,17 @@ public class InquiryMenu {
 	
 	// 1대1문의 작성
 		public int inquiry() {
-			System.out.println("=========== 1대1문의 =============");
-			System.out.print("문의 제목>");
+			System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+			System.out.println("■■■■■■■■■■■■■■■■■■■■1:1문의 작성 ■■■■■■■■■■■■■■■■■■■■■■");
+			System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+			System.out.println("문의 작성을 시작합니다.");
+			System.out.print("☞  문의 제목>");
 			String inquirytitle = ScanUtil.nextLine();
-			System.out.print("문의 이메일>");
+			System.out.print("☞  문의 이메일>");
 			String inquiryemail = ScanUtil.nextLine();
-			System.out.print("문의 전화번호>");
+			System.out.print("☞  문의 전화번호>");
 			String inquirynumber = ScanUtil.nextLine();
-			System.out.print("문의 내용>");
+			System.out.print("☞  문의 내용>");
 			String inquirycontent = ScanUtil.nextLine();
 
 			Map<String, Object> param = new HashMap<>();
@@ -44,13 +47,13 @@ public class InquiryMenu {
 
 			int result = myDao.insertInquiry(param);
 
-			// 영향 받은 행이 잇다면, 등록성공 메시지가 출력되고 그것이 아니면 실패 메시지가 생성된다.
+			// 영향 받은 행이 있다면 등록성공 메시지가 출력되고 아니면 실패 메시지가 출력된다.
 			if (0 < result) {
-				System.out.println("1대1문의 등록 성공");
+				System.out.println("1:1문의 등록 성공");
 			} else {
-				System.out.println("1대1문의 등록 실패");
+				System.out.println("1:1문의 등록 실패");
 			}
-			// 등록 완료되면 마이페이지로 이동된다.
+			// 등록 완료되면 비로그인은 HOME, 로그인은 마이페이지 메뉴로 돌아간다.
 			if (Controller.loginUser == null){
 				return View.HOME;
 			}else{
